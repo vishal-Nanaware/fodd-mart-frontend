0e0a1594116d1e90cd718f899c81d400639ad41d
vishal-Nanaware <vishalnanaware7850@gmail.com>
2024-06-29 23:59:04 +0530

    modify admin page

--- admin/admin.js
+++ admin/admin.js
@@ -1,109 +1,66 @@
-const data = [
-  {
-    name: "Hamburger",
-    description:
-      "A sandwich consisting of one or more cooked patties of ground meat, usually beef, placed inside a sliced bread roll or bun.",
-    img: "img/hamburger.jpg",
-    origin: "United States",
-    price: 8.99,
-    calories: 550,
-  },
-  {
-    name: "Apple Pie",
-    description:
-      "A dessert consisting of a pastry crust filled with sliced apples, sugar, and spices.",
-    img: "img/applePie.jpg",
-    origin: "United States",
-    price: 4.99,
-    calories: 320,
-  },
-  {
-    name: "Pancakes",
-    description:
-      "A flat cake, often thin and round, prepared from a starch-based batter and cooked on a hot surface.",
-    img: "img/pancakes.jpg",
-    origin: "United States",
-    price: 5.99,
-    calories: 350,
-  },
+const offerDiv = document.getElementById("offer-div");
+const usersDiv = document.getElementById("users-div");
+const productDiv = document.getElementById("product-div");
 
-  {
-    name: "Macaroni and Cheese",
-    description:
-      "A dish of cooked macaroni pasta and a cheese sauce, typically cheddar.",
-    img: "img/macoroni.jpg",
-    origin: "United States",
-    price: 7.49,
-    calories: 450,
-  },
-];
-
-const offerDiv = document.getElementById('offer-div');
-const usersDiv = document.getElementById('users-div')
-const productDiv = document.getElementById('product-div')
-
-
-
-document.getElementById("offerList").addEventListener('click', ()=>{
-    displayDiv(offerDiv)
-    
-
-})
+document.getElementById("offerList").addEventListener("click", () => {
+  displayDiv(offerDiv);
+});
 document.getElementById("userList").addEventListener("click", () => {
   displayDiv(usersDiv);
 });
 document.getElementById("productList").addEventListener("click", () => {
   displayDiv(productDiv);
-
 });
 
-
-function hideDiv(){
-    offerDiv.style.display = "none"
-    usersDiv.style.display = "none";
-    productDiv.style.display = "none";
+function hideDiv() {
+  offerDiv.style.display = "none";
+  usersDiv.style.display = "none";
+  productDiv.style.display = "none";
 }
-function displayDiv(div){
-    hideDiv()
-    div.style.display = "block"
+function displayDiv(div) {
+  hideDiv();
+  div.style.display = "block";
 }
 
-function createCard(item){
-  let card= document.getElementById("cardDiv");
-  let child = document.createElement("div");
-  let itemName = document.createElement("h3");
-  let imgDiv = document.createElement('div')
-  let itemPrice =  document.createElement('h3')
-  let itemDescription = document.createElement('p')
+function createTable(item) {
+  let table = document.getElementsByTagName('table')
+  let tr = document.createElement('tr')
+  let td_id = document.createElement('td')
+  let td_category = document.createElement("td");
+  let td_Name = document.createElement("td");
+  let td_Price = document.createElement("td");
+  let td_calories = document.createElement("td");
+  let td_description = document.createElement("td");
 
-  itemName.innerText = item.name
-  itemPrice.innerText = `$ ${item.price}`
-  let img = document.createElement('img');
-  img.src = item.img
-  img.height = "100"
-  console.log("her")
-  child.classList.add("card")
-  itemName.classList.add("itemName")
-  imgDiv.classList.add("imgdiv")
-  imgDiv.appendChild(img)
-  itemDescription.innerText = item.description;
-  itemDescription.classList.add("itemDescription");
+  td_Name.innerText = item.name
+  td_id.innerText = item.id
+  td_description.innerText = item.description
+  td_category.innerText = item.origin
+  td_Price.innerText = item.price
+  td_calories.innerText = item.calories
 
-  card.append(child)
-  child.append(itemName)
-  child.append(imgDiv)
-  child.append(itemPrice)
-  child.append(itemDescription)
-
-
-
+  tr.append(td_id , td_calories , td_Name, td_Price, td_calories, td_description) 
+  table.appendchild(tr)
 
 }
-function getProducts(data){
- for(i=0;i<data.length;i++){
-  console.log(data[i])
-  createCard(data[i])
 
- }
+//fetching data
+
+function formTable(data){
+  for(i = 0;i<data.lenght;i++){
+    
+  }
 }
- getProducts(data);
+
+function getProducts() {
+  try {
+    fetch("http://localhost:3000/admin")
+      .then((res) => res.json())
+      .then((data) =>{
+        console.log(data)
+
+      });
+  } catch (e) {
+    console.log(e);
+  }
+}
