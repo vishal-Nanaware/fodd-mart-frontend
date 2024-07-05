383341dde2c56d388d1ba5ab48653ea27dd737c3
vishal-Nanaware <vishalnanaware7850@gmail.com>
2024-07-05 23:36:32 +0530

    fix getproduct and sendProduct dunc

--- item/item.js
+++ item/item.js
@@ -1,25 +1,34 @@
-function getData(ProductId) {
+async function getData(ProductId) {
   try {
-    fetch(`http://localhost:3000/product/ProductId?ProductId=${ProductId}`, {
-      method: "POST",
-      headers: {
-        "Content-type": "application/json; charset=UTF-8",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        render(data);
-        console.log(data);
-      });
+    const response = await fetch(
+      `http://localhost:3000/product/ProductId?ProductId=${ProductId}`,
+      {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }
+    );
+    const data = await response.json();
+    console.log("Data fetched:", data);
+    return data;
   } catch (e) {
     console.log("something went wrong");
   }
 }
 
-function getProductid() {
+async function getProductid() {
   let ProductId = sessionStorage.getItem("ProductId");
   if (ProductId) {
-    getData(ProductId);
+    try {
+      let data = await getData(ProductId);
+      console.log(data, " :from getProduct");
+      render(data);
+    } catch (e) {
+      console.log("Error fetching data:", e);
+    }
+  } else {
+    console.log("No ProductId found in sessionStorage.");
   }
 }
 getProductid();
@@ -31,8 +40,15 @@ function checkUserToken(value) {
   return false;
 }
 
-function order(e) {
-  addProduct()
+async function orderBtn(e) {
+  console.log(e.target.id);
+  try {
+    let data = await getData(e.target.id);
+    console.log(data, ": order");
+    addValueToForm(data);
+  } catch (e) {
+    console.log("something went wrong");
+  }
 }
 
 function render(data) {
@@ -42,27 +58,115 @@ function render(data) {
   document.getElementById("price").innerText = `$ ${data.productPrice}`;
   document.getElementById("cal").innerText = `${data.productCalrie} Cal`;
   document.getElementById("orderBtn").setAttribute("id", data._id);
-  document.querySelector(".orderBtn").addEventListener("click", order);
+  document.querySelector(".orderBtn").addEventListener("click", orderBtn);
 }
 
+function checkUserToken(value) {
+  // if (value) {
+  //   return true;
+  // }
+  // return false;
+  return true;
+}
+
+function addValueToForm(data) {
+  let userTokencExist = checkUserToken(localStorage.getItem("token"));
+
+  if (userTokencExist) {
+    document.getElementById("ProductNameValue").innerText = data.productName;
+    document.getElementById("formPriceValue").innerText = data.productPrice;
+    document.getElementById("formCalValue").innerText = data.productCalrie;
+    const quantity = document.getElementById("counterValue").innerText;
+    document.getElementById("formQuaValue").innerText = quantity;
+    document.getElementById("ProductNameValue").setAttribute("value", data._id);
+    addProduct();
+  } else {
+    alert("Need a sign Up for Order");
+  }
+}
 function addProduct() {
   const form = document.getElementById("form");
+
   if (form.style.display === "block") {
     form.style.display = "none";
     return;
   }
   form.style.display = "block";
 }
-function counter(value){
- 
-  if(value == "increase"){
-   let count =  document.getElementById("counterValue").innerText ;
-   count++
-   document.getElementById("counterValue").innerText = count 
-  }else{
-      let count = document.getElementById("counterValue").innerText;
-      if(count == 1) return
-      count--;
-      document.getElementById("counterValue").innerText = count;
+function counter(value) {
+  const counter = document.getElementById("counterValue");
+  if (value == "increase") {
+    let count = counter.innerText;
+    count++;
+    counter.innerText = count;
+  } else {
+    let count = counter.innerText;
+    if (count == 1) return;
+    count--;
+    counter.innerText = count;
   }
 }
+
+
+
+
+//order Confirm
+document.addEventListener("DOMContentLoaded", (e) => {
+  const form = document.getElementById("form");
+
+  form.addEventListener("submit",async (e) => {
+    e.preventDefault(); // Prevent the form from submitting the traditional way
+
+    const formData = {};
+
+    for (let i = 0; i < form.elements.length; i++) {
+      const element = form.elements[i];
+      console.log(element," : from form data")
+      formData[element.id] = element.value;
+
+      // element.value = "";
+    }
+
+    let id = document.getElementById("ProductNameValue").getAttribute('value');
+    console.log(id)
+    // let data = await getData(id);
+    // console.log(data)
+    formData.productId = id
+    
+    console.log("Form Data:" , formData);
+    console.log(localStorage.getItem('token'))
+    sendOrder(formData)
+  });
+});
+
+function sendOrder(data){
+  const token = localStorage.getItem("token");
+  fetch(`http://localhost:3000/user/order`,{ 
+        method: "POST",
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+        body:JSON.stringify({
+          token:token,
+          userOrder:data
+        })
+  }).then(res => res.json()).then(data => console.log(data));
+}
+
+//userCheck
+function userlogged() {
+  let user = usercheck();
+  if (user) {
+   document.getElementById("userDetails").innerHTML = user
+  }
+
+}
+
+function usercheck() {
+  let user = localStorage.getItem("user");
+  if (!user) {
+    return false;
+  }
+  return user;
+}
+userlogged();
